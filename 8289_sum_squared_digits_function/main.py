e85fa95de643ddcc9f34b71129f0a2e682a85673
Avery <avery.erwinmcguire@gmail.com>
2018-05-22 16:48:19 -0400

    solved problem 8289
    co-authored by: data-pup

--- 8289_sum_squared_digits_function/main.py
+++ 8289_sum_squared_digits_function/main.py
@@ -12,29 +12,40 @@
 
 '''
 
-def calculateIntegerInBase(base, integerVal):
+def squareSum(digitList):
+    runningSum = 0
+    for i in digitList:
+        runningSum+=pow(i,2)
+    return runningSum
 
+
+def calculateIntegerInBase(base, integerVal):
     baseIndex = 0
     while(True):
         if (pow(base,baseIndex) > integerVal):
             pass # begin algorithm to check lesser powers
-            print baseIndex
             break
         baseIndex+=1
     baseIndex-=1
 
     remaining = integerVal
     digits = []
+    
     while baseIndex >= 0:
         nextDigit = base - 1
         nextAmount = nextDigit * pow(base, baseIndex)
         while nextAmount > remaining:
             nextDigit -= 1
+            nextAmount = nextDigit * pow(base, baseIndex)
         digits.append(nextDigit)
         remaining -= nextAmount
+        baseIndex-=1
+    return digits
 
 if __name__ == '__main__':
     input1 = (1, 10, 1234)
     input2 = (2, 3, 98765)
     input3 = (3, 10, 987654321)
-    calculateIntegerInBase(10,1234)
+
+    digitlist = calculateIntegerInBase(16,987654321)
+    print (squareSum(digitlist))
