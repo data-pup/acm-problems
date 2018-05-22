852524915535f81d8a781346fc542e8a67c1ebce
data-pup <kevinmartinucr@gmail.com>
2018-05-22 13:36:31 -0700

    Added some next steps for solving problem 8289.
    Co-authored by: averyEMcG <avery.erwinmcguire@gmail.com>

--- 8289_sum_squared_digits_function/main.py
+++ 8289_sum_squared_digits_function/main.py
@@ -8,28 +8,33 @@
 
 
 -> take the base number, and incrementally check each power to determine if it exceeds the integer
--> once it exceeds, then 
+-> once it exceeds, then
 
 '''
 
 def calculateIntegerInBase(base, integerVal):
+
     baseIndex = 0
-    
     while(True):
         if (pow(base,baseIndex) > integerVal):
             pass # begin algorithm to check lesser powers
             print baseIndex
             break
-
         baseIndex+=1
+    baseIndex-=1
 
-    
-            
+    remaining = integerVal
+    digits = []
+    while baseIndex >= 0:
+        nextDigit = base - 1
+        nextAmount = nextDigit * pow(base, baseIndex)
+        while nextAmount > remaining:
+            nextDigit -= 1
+        digits.append(nextDigit)
+        remaining -= nextAmount
 
-        
-input1 = (1, 10, 1234) 
-input2 = (2, 3, 98765) 
-input3 = (3, 10, 987654321) 
-calculateIntegerInBase(10,1234)
-
-
+if __name__ == '__main__':
+    input1 = (1, 10, 1234)
+    input2 = (2, 3, 98765)
+    input3 = (3, 10, 987654321)
+    calculateIntegerInBase(10,1234)
